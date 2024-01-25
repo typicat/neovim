891b4c479dabc019792a298a6d032a399d501c53
Karl Jonsson <kjonsson@gmail.com>
2024-01-25 18:46:57 +0100

    first

---
+++ init.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+local g = vim.g
+
+opt.number=true
+opt.cursorline=true
+opt.mouse='a'
+opt.backspace="eol,indent,start"
+opt.clipboard='unnamedplus'
+opt.cmdheight=0
+opt.laststatus=2
+
+opt.tabstop=2
+opt.shiftwidth=2
+opt.expandtab=true
+opt.smarttab=true
+
+opt.autoindent=true
+opt.smartindent=true
+
+opt.updatetime=300
+opt.hidden=true
+opt.lazyredraw=true
