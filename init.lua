efe0761ca859102ecefb3c5cf246461e81a173b5
Karl Jonsson <kjonsson@gmail.com>
2024-01-25 21:11:37 +0100

    second

--- init.lua
+++ init.lua
@@ -1,22 +1,24 @@
-local opt = vim.opt
-local g = vim.g
-
-opt.number=true
-opt.cursorline=true
-opt.mouse='a'
-opt.backspace="eol,indent,start"
-opt.clipboard='unnamedplus'
-opt.cmdheight=0
-opt.laststatus=2
-
-opt.tabstop=2
-opt.shiftwidth=2
-opt.expandtab=true
-opt.smarttab=true
-
-opt.autoindent=true
-opt.smartindent=true
-
-opt.updatetime=300
-opt.hidden=true
-opt.lazyredraw=true
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "nvim-treesitter/nvim-treesitter",
+  "echasnovski/mini.nvim",
+})
+require("nvim-treesitter.configs").setup({
+   ensure_installed = {"lua", "python", "c"},
+   highlight = {enable=true,},
+  })
+require("mini.pairs").setup({})
+require("mini.statusline").setup({})
+require("mini.indentscope").setup({})
+require("options")
